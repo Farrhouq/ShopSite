9ca3b7ff5205b74e537342d11562eb965076f057
wodoame <bmwodoame@gmail.com>
2022-12-08 06:54:25 -0800

    created views, added templates, general styling

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views 
+
 
 urlpatterns = [
-    
+    path('',views.dashboard, name='dashboard'), 
+    path('signin/', views.signin, name='signin'), 
+    path('signup/', views.signup, name='signup'), 
+    path('create/', views.create_shop, name='create_shop'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    context = {}
+    return render(request, 'main/dashboard.html', context)
+
+
+def signin(request):
+    context = {}
+    return render(request, 'main/signin.html', context)
+
+
+def signup(request):
+    context = {}
+    return render(request, 'main/signup.html', context)
+
+
+def create_shop(request):
+    context = {}
+    return render(request, 'main/create_shop.html', context)
+ 
